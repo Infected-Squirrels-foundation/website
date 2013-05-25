90e14b9e84bcce2df4e6fdbfdc23403fd7f88fd1
Artem Suschev <Suschev.Artem@gmail.com>
2013-05-25 22:15:36 +0400

    angry textarea

--- index.py
+++ index.py
@@ -1,11 +1,11 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+	print url_for('static', filename='main.css')
+	return render_template('index.html')
 
 if __name__ == "__main__":
 	app.debug = True
